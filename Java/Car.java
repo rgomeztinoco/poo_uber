2270d4ecf9112f4870b4b5ccac3af2ff38772954
rgomeztinoco <gricardo2213@gmail.com>
2021-09-26 16:57:37 -0500

    construyendo y mostrando en Java

--- Java/Car.java
+++ Java/Car.java
@@ -3,10 +3,15 @@ package Java;
 public class Car {
   Integer id;
   String license;
-  String driver;
+  Account driver;
   Integer passenger;
 
+  public Car(String license, Account driver) {
+    this.license = license;
+    this.driver = driver;
+  }
+
   void printDataCar() {
-    System.out.println("License: " + license + "  Driver: " + driver);
+    System.out.println("License: " + license + "  Drivers name: " + driver.name);
   }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -3,15 +3,13 @@ package Java;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
-    Car car = new Car();
-    car.license = "AMQ123";
-    car.driver = "Juan Pancracio";
+
+    Car car = new Car("AMQ123", new Account("Juan Pancracio", "1235464521"));
     car.passenger =4;
     car.printDataCar();
-    Car car2 = new Car();
-    car2.license = "HASDF123";
-    car2.driver = "Andrea Herrera";
+
+    Car car2 = new Car("HASDF123", new Account("Andrea Herrera", "5468781"));
     car2.passenger = 3;
-    System.out.println("Car License: " + car2.license);
+    car2.printDataCar();
   }
 }
