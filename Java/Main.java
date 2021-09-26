f095e6c196b4f97641bbbe93e72d823cc1003640
rgomeztinoco <gricardo2213@gmail.com>
2021-09-26 15:09:06 -0500

    constructores y funciones dentro de clases java

--- Java/Main.java
+++ Java/Main.java
@@ -3,5 +3,15 @@ package Java;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
+    Car car = new Car();
+    car.license = "AMQ123";
+    car.driver = "Juan Pancracio";
+    car.passenger =4;
+    car.printDataCar();
+    Car car2 = new Car();
+    car2.license = "HASDF123";
+    car2.driver = "Andrea Herrera";
+    car2.passenger = 3;
+    System.out.println("Car License: " + car2.license);
   }
 }
